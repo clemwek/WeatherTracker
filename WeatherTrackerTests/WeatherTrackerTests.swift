a40a6bf7482b174561ae77d3c116368b88f30593
Clement <clemwek@gmail.com>
2024-12-19 08:43:11 +0300

    Initial Commit

---
+++ WeatherTrackerTests/WeatherTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTrackerTests.swift
+//  WeatherTrackerTests
+//
+//  Created by Clement  Wekesa on 12/19/24.
+//
+
+import Testing
+@testable import WeatherTracker
+
+struct WeatherTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
